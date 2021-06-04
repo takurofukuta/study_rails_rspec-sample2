daf43b9f67d818b5ab3313d54c7a484d76ea303a
takurofukuta <mojuraspax@icloud.com>
2021-06-04 22:51:43 +0900

    updateのリクエストスペックを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user
+    else
+      render :edit
+    end
   end
 
   def destroy
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -128,4 +128,64 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+  describe 'PATCH #update' do
+    subject { patch(user_path(user.id), params: params) }
+    let(:user) { create(:user) }
+
+    context 'パラメータが正常な場合' do
+      let(:params) { { user: attributes_for(:user) } }
+
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it 'name が更新される' do
+        origin_name = user.name
+        new_name = params[:user][:name]
+        expect { subject }.to change { user.reload.name }.from(origin_name).to(new_name)
+      end
+
+      it 'age が更新される' do
+        origin_age = user.age
+        new_age = params[:user][:age]
+        expect { subject }.to change { user.reload.age }.from(origin_age).to(new_age)
+      end
+
+      it 'email が更新される' do
+        origin_email = user.email
+        new_email = params[:user][:email]
+        expect { subject }.to change { user.reload.email }.from(origin_email).to(new_email)
+      end
+      it '詳細ページにリダイレクトされる' do
+        subject
+        expect(response).to redirect_to User.last
+      end
+    end
+    context 'user のパラメータが異常なとき' do
+      let(:params) { { user: attributes_for(:user, :invalid) } }
+
+      it 'リクエストが成功する' do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it 'name が更新されない' do
+        expect { subject }.not_to change(user.reload, :name)
+      end
+
+      it 'age が更新されない' do
+        expect { subject }.not_to change(user.reload, :age)
+      end
+
+      it 'email が更新されない' do
+        expect { subject }.not_to change(user.reload, :email)
+      end
+
+      it '編集ページがレンダリングされる' do
+        subject
+        expect(response.body).to include '編集'
+      end
+    end
+  end
 end
